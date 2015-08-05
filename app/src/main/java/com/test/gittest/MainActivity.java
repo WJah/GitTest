1af13ec13b82c8d14f7c04ce5113d67bd0ed2363
Jah <wangjiahe2009@gmail.com>
2015-08-05 22:41:23 +0800

    设置MainActivity按钮时间为启动SecondActivity

--- app/src/main/java/com/test/gittest/MainActivity.java
+++ app/src/main/java/com/test/gittest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.test.gittest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -19,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
             }
         });
     }
