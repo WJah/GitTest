20c093fe7ee2011716515ba77a8de7da8dccadf8
Jah <wangjiahe2009@gmail.com>
2015-08-06 00:02:30 +0800

    为SecondActivity添加Switch控件

--- app/src/main/java/com/test/gittest/SecondActivity.java
+++ app/src/main/java/com/test/gittest/SecondActivity.java
@@ -4,13 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Switch;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private Switch mSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        mSwitch = (Switch)findViewById(R.id.switch1);
     }
 
     @Override
